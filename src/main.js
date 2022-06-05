bcb41a22c5da151298174a0831fe54a7781c72d8
yhy <784354821@qq.com>
2022-06-05 14:52:09 +0800

    解决打包出错

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
  * @Author: yhy
  * @Date: 2022-06-05 11:10:02
  * @LastEditors: yhy
- * @LastEditTime: 2022-06-05 14:20:12
+ * @LastEditTime: 2022-06-05 14:38:31
  * @Description:
  */
 import { createApp } from "vue";
 import App from "./App.vue";
-import ygUiPlus from "../packages";
+// import ygUiPlus from "../packages";
+import ygUiPlus from "yg-ui-plus";
 
 let app = createApp(App);
 app.use(ygUiPlus);
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: yhy
  * @Date: 2022-06-05 11:10:02
  * @LastEditors: yhy
- * @LastEditTime: 2022-06-05 14:19:59
+ * @LastEditTime: 2022-06-05 14:47:25
  * @Description:
  */
 import { defineConfig } from "vite";
@@ -13,6 +13,16 @@ import { join } from "path";
 export default defineConfig({
   plugins: [vue()],
   build: {
+    rollupOptions: {
+      // 请确保外部化那些你的库中不需要的依赖,解决报（reading ‘isCE‘）错误
+      external: ["vue"],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
     lib: {
       entry: join(__dirname, "./packages/index.js"),
       name: "ygUiPlus",
